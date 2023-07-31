ea296b97ff7d026535d563136a8c3ebe7d8456e3
alexa <alexanderredar1@gmail.com>
2023-07-31 16:43:21 +0300

    [8] - Аннотация @RequestMapping для Controller класса

--- src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
 
     @RequestMapping("/")
@@ -19,22 +20,6 @@ public class MyController {
     public String askEmployeeDetails() {
         return "ask-emp-details-view";
     }
-
-//    @RequestMapping("/showDetails")
-//    public String showEmpDetails() {
-//        return "show-emp-details-view";
-//    }
-
-//    @RequestMapping("/showDetails")
-//    public String showEmpDetails(HttpServletRequest request, Model model) {
-//
-//        String empName = request.getParameter("employeeName");
-//        empName = "Mr. " + empName;
-//        model.addAttribute("nameAttribute", empName);
-//        model.addAttribute("description", " - udemy instructor");
-//
-//        return "show-emp-details-view";
-//    }
 
     @RequestMapping("/showDetails")
     public String showEmpDetails(@RequestParam("employeeName") String empName
